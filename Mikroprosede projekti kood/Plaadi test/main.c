a2078e2dc3fb6f8fc9ece3299bd7850c4e21c21f
andres põldaru <andreseoma@hotmail.com>
2016-06-14 21:46:37 +0300

    Latest version, works allright.

--- Mikroprosede projekti kood/Plaadi test/main.c
+++ Mikroprosede projekti kood/Plaadi test/main.c
@@ -4,14 +4,17 @@
  * Created: 30.05.2016 20:51.19
  * Author : Andres
  */ 
-#define F_CPU 1000000
+#define F_CPU 16000000
+
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/power.h>
 #include <util/delay.h>
 #include <avr/eeprom.h>
+#include "usb_serial.h"
 
+//various pin definitions, for easier readability
 #define LED1 (1<<PORTD4)
 #define LED2 (1<<PORTD6)
 #define LED3 (1<<PORTD7)
@@ -29,12 +32,14 @@
 #define NGOE2 (1<<PORTB5)
 #define NBOE2 (1<<PORTB4)
 
-uint16_t rotationLengthCounter;
-uint8_t rotationOverflow;
+void usbSendString(char* string);
+void receiveDataFromUSB();
+
+uint8_t rotationOverflow; //flags if the rotation speed is too slow and the period counter overflows
 uint8_t screenStep; //which step of the screen we are currently at
-uint8_t screenData[128][6];
-uint8_t screenClearData[6] = {0};
-uint8_t screenTestData[6] = {0,0,0,1<<7,1<<7,1<<7};
+uint8_t screenData[128][6]; //the data that is displayed, 128 segments
+uint8_t screenClearData[6] = {0}; //used for clearing the screen when needed
+uint8_t screenTestData[6] = {0,0,0,1<<7,1<<7,1<<7}; //used for testing
 
 void shift(uint8_t data[6]){
 	//shift the 6 bytes of data out to the led drivers, the data is in the order RGB, leds start counting from the center outwards
@@ -87,12 +92,13 @@ void shift(uint8_t data[6]){
 }
 
 ISR(TIMER3_COMPA_vect){
+	//update the screen, the timer interrupts 128 times each rotation
 	if(screenStep < 128){
 		shift(screenData[screenStep]);
 		++screenStep;
 	}
 	else{
-		shift(screenClearData);
+		//shift(screenClearData);
 		TIMSK3 = 0;
 	}
 }
@@ -100,23 +106,59 @@ ISR(TIMER3_COMPA_vect){
 ISR(TIMER1_OVF_vect){
 	//too much time passed to measure the rotation period, rotation < 4Hz
 	rotationOverflow = 1;
-	shift(screenTestData); //light up, to show that the device is working
-	TIMSK3 = 0; //turn off screen updates
+	shift(screenTestData); //light up one led, to show that the device is working
+	TIMSK3 = 0; //turn off screen updates by disabling the screen update interrupts
 }
 
 ISR(INT0_vect){
 	//magnet detected
-	PINC |= LED5; //for debugging flash a led on the underside
+	//PINC |= LED5; //for debugging purposes flash a led on the underside
 	if(!rotationOverflow){
-		//start the screen updating again, the rotation period didn't overflow and we can use the period
+		//start the screen updating again, the rotation period didn't overflow and we can trust the period value
 		TIMSK3 = 1<<OCIE3A;
 	}
 	//update the counter so that we will get an interrupt 128 times per second given the last TCNT1 value, zero the rotation period counter
 	OCR3A = TCNT1>>1;
-	TCNT3 = OCR3A-1;
-	TCNT1 = 0;
-	rotationOverflow = 0;
-	screenStep = 0;
+	TCNT3 = OCR3A-1; //this gives us an interrupt on the next counter increase
+	TCNT1 = 0; //zero the period counter
+	rotationOverflow = 0; //zero the period timer overflow flag
+	screenStep = 0; //we are at screen segment 0
+}
+
+void usbSendString(char* string){
+	//a function used for sending strings with usb
+	uint8_t len = 0; //let's find the length of the string
+	while(string[len] != 0){
+		++len;
+	}
+	if(len != 0){
+		usb_serial_write((uint8_t*)string, len);
+	}
+}
+
+void readDataFromEeprom(){
+	//this reads the data from eeprom into RAM during startup
+	eeprom_read_block(screenData, 0, 6*128);
+}
+
+void receiveDataFromUSB(){
+	//this function receives via usb 6 bytes of data for each of the 128 segments and saves it to eeprom
+	for(uint8_t n = 0; n < 128; ++n){
+		for(uint8_t m = 0; m < 6; ++m){
+			while(!usb_serial_available()); //wait for a char
+			int16_t c = usb_serial_getchar();
+			if(c == -1){
+				usbSendString("ATMEGA USB RECEIVE ERROR\n");
+			}
+			screenData[n][m] = c&0xFF; 
+		}
+	}
+	
+	//send an acknowledgement back, that data has been received and write it to the eeprom
+	usbSendString("Data received\n");
+	usbSendString("Writing to eeprom...\n");
+	eeprom_write_block(screenData, 0, 6*128);
+	usbSendString("Eeprom written\n");
 }
 
 int main(void)
@@ -131,9 +173,6 @@ int main(void)
 	DDRD = LED1 | LED2 | LED3;
 	DDRC = LED4 | LED5;
 	
-	PORTD = LED1|LED2|LED3;
-	PORTC = LED4|LED5;
-	
 	//enable the pins that control the led drivers
 	DDRF |= NROE1|NGOE1|NBOE1|LE1;
 	DDRB = NROE2|NGOE2|NBOE2|LE2;
@@ -147,32 +186,27 @@ int main(void)
 	TCCR1B = 0b011; //prescaler 64
 	TIMSK1 = 1<<TOIE1;
 	
-	//this timer gives a compare event 64 times each rotation, so that we can update the states of the leds
+	//this timer gives a compare event 128 times each rotation, so that we can update the states of the leds
 	TCCR3B = 0b001 | (1<<WGM12); //prescaler 1, CTC
-	OCR3A = TCNT1;
+	OCR3A = TCNT1>>1; //divide by two so that we get 128 interrupts
 	TIMSK3 = 1<<OCIE3A;
 	
 	//magnet sensor interrupt
 	EICRA = 0b10;
 	EIMSK = 1;
 	
+	PORTD |= LED1; //these leds are on the other side, for testing purposes
+	PORTD |= LED2;
+	readDataFromEeprom(); //read the saved image data from the eeprom
 	
-	screenData[61][4] = 1<<6;
-	screenData[62][4] = 1<<7;
-	screenData[63][4] = 1<<5;
-	
-	for(uint8_t n = 0; n < 128; ++n){
-		screenData[n][4] = 1<<(n%8);
-	}
-	
-	screenData[0][3] |= 2;
-	
+	usb_init(); //init usb
+	while(!usb_configured());
 	sei();
-	
-	//uint8_t testdata[6]={1,2,4,1,2,4};
-	//shift(testdata);
 	
     while (1) 
     {
+		if(usb_serial_available()){ //check is data transmission is being started from the usb
+			receiveDataFromUSB();
+		}
     }
 }
--- Pildi generaator/main.cpp
+++ Pildi generaator/main.cpp
@@ -13,9 +13,16 @@
 #include <QPen>
 #include <QLine>
 #include <QPoint>
+#include <QFileDialog>
+#include <QColor>
 #include <vector>
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include "Serial.h"
+
+int qRed(QRgb rgb);
+int qGreen(QRgb rgb);
+int qBlue(QRgb rgb);
 
 class WCwindow : public QMainWindow
 {
@@ -23,11 +30,13 @@ class WCwindow : public QMainWindow
 public:
 	WCwindow();
 	bool eventFilter(QObject *obj, QEvent *event);
-	void drawCircle(int x, int y, int radius, QRgb color);
-	void drawCircle2(int x, int y, int radius, QRgb color);
-	void drawSegment(int x, int y, int radius, QRgb color);
-	void drawSegment2(int x, int y, int diameter, QRgb color);
+	void drawSegmentXY(int x, int y, int diameter, QRgb color, Qt::PenCapStyle = Qt::RoundCap);
+	void drawSegment(int angleStep, int lengthStep, int diameter, QRgb color, Qt::PenCapStyle penCapStyle = Qt::RoundCap);
 	void textOut(QString text1);
+	void clearImageNoPaint();
+	void clearAVRData();
+	int WCwindow::getColorBit(QRgb col, int colornum);
+	void WCwindow::getUSBCommand(char* destination);
 
 	QGridLayout *layout;
 	QGridLayout *rightSidePanel;
@@ -41,11 +50,13 @@ public:
 	QCheckBox *checkBoxRed;
 	QCheckBox *checkBoxGreen;
 	QCheckBox *checkBoxBlue;
-	QRgb pictureDataAVR[128][16];
-	QRgb activeColor = qRgb(100, 100, 100);
+	QRgb pictureDataAVR[128][16] = {};
+	QRgb activeColor;
 	int ledPixelCount = 16;
-	int imageSize = (ledPixelCount+1) * 31;
+	int imageSize = ledPixelCount * 32;
 	QLineF *mouseMovementLine = new QLineF();
+	int imageLEDLightDiameter = 7;
+	CSerial serial;
 
 public slots:
 	void handleButtonSend();
@@ -57,6 +68,7 @@ public slots:
 
 WCwindow::WCwindow()
 {
+	//make the window, make all the GUI elements, put them in layouts, register event handlers
 	buttonSend = new QPushButton("Send", this);
 	buttonClear = new QPushButton("Clear", this);
 	buttonSave = new QPushButton("Save", this);
@@ -82,18 +94,11 @@ WCwindow::WCwindow()
 	text->insertPlainText("Testing\n");
 	text->setMinimumHeight(200);
 	
-	for (int x = 0; x < image->width(); ++x) {
-		for (int y = 0; y < image->height(); ++y) {
-			if ((x - imageSize / 2)*(x - imageSize / 2) + (y - imageSize / 2)*(y - imageSize / 2) <= (ledPixelCount * 16)*(ledPixelCount * 16)) {
-				image->setPixel(x, y, qRgba(0, 0, 0, 255));
-			}
-			else {
-				image->setPixel(x, y, qRgba(0, 0, 0, 0));
-			}
-		}
-	}
+	handleButtonClear();
 
-	imageHolder->setPixmap(QPixmap::fromImage(*image));
+	checkBoxRed->setChecked(false);
+	checkBoxGreen->setChecked(true);
+	checkBoxBlue->setChecked(false);
 
 	layout->addWidget(imageHolder, 0, 0);
 	layout->addWidget(text, 2, 0, 1, 2);
@@ -119,29 +124,144 @@ WCwindow::WCwindow()
 	setWindowTitle("Pildigeneraator");
 	imageHolder->setMouseTracking(true);
 	imageHolder->installEventFilter(this);
+}
 
+int WCwindow::getColorBit(QRgb col, int colorNum) {
+	//used in generating the data that the microcontroller wants, returns whether the given color is not zero
+	int returnValue;
+	if (colorNum == 0) {
+		returnValue = qRed(col);
+	}
+	else if (colorNum == 1) {
+		returnValue = qGreen(col);
+	}
+	else if (colorNum == 2) {
+		returnValue = qBlue(col);
+	}
+	if (returnValue) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+void WCwindow::getUSBCommand(char* destination) {
+	//reads up to newline from USB
+	while (1) {
+		while (!serial.ReadDataWaiting());
+		serial.ReadData(destination, 1);
+		++destination;
+		if (*(destination-1) == '\n') {
+			*destination = 0;
+			return;
+		}
+	}
 }
 
 void WCwindow::handleButtonSend() {
-	textOut(QString("Send\n"));
+	//generates the data to send to the microcontroller and sends it, avrData is in the same format as the microcontroller uses
+	//the picturedata is in the pictureDataAVR array, but it is in a different format, because it has a QRgb for every 16 leds of the 128 segments,
+	//but the microcontroller wants for each segment a 6 byte value with each bit in the 6 bytes representing each led
+	textOut("Generating data\n");
+	char avrData [128][6] = { 0 };
+	for (int n = 0; n < 128; ++n) {
+		for (int m = 0; m < 3; ++m) {
+			for (int k = 0; k < 16; ++k) {
+				avrData[n][m + (k >= 8) * 3] |= getColorBit(pictureDataAVR[n][k], m)<<(k%8);
+			}
+		}
+	}
+	textOut("Sending data\n");
+	if (serial.Open(3, 115200)) {
+		serial.SendData((char*)avrData, 6*128);
+		char data[64] = { 0 };
+		while (1) {
+			//wait for the Eeprom to be written
+			getUSBCommand(data);
+			textOut(data);
+			if (strcmp(data, "Eeprom written\n") == 0) {
+				break;
+			}
+		}
+		textOut("Sending end.\n\n");
+		serial.Close();
+	}
+	else {
+		textOut("COM port not open.\n");
+	}
 }
-void WCwindow::handleButtonClear() {
+void WCwindow::clearAVRData() {
+	//for zeroing the array with the segment data
+	for (int ang = 0; ang < 128; ++ang) {
+		for (int led = 0; led < 16; ++led) {
+			pictureDataAVR[ang][led] = qRgb(0, 0, 0);
+		}
+	}
+}
+void WCwindow::clearImageNoPaint() {
 	//clear the image
 	image->fill(QColor(0, 0, 0, 0));
 	QPainter painter(image);
-	painter.setPen(QPen(QColor(0,0,0,255), ledPixelCount*16*2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+	painter.setPen(QPen(QColor(0, 0, 0, 255), imageSize, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
 	painter.drawPoint(imageSize / 2, imageSize / 2);
-	
+}
+void WCwindow::handleButtonClear() {
+	//clear the image and display the cleared image
+	clearImageNoPaint();
+	clearAVRData();
 	imageHolder->setPixmap(QPixmap::fromImage(*image));
 }
 void WCwindow::handleButtonSave() {
-	textOut(QString("Save\n"));
+	//save the current image to an image file
+	QString filename = QFileDialog::getSaveFileName(this, "Select the image file", "", "Images (*.png *.jpg *.jpeg *.bmp)");
+	if (!filename.isEmpty()) {
+		//make a new image, where we draw with a flat cap, to minimize rounding errors
+		QImage image2 = *image;
+		clearImageNoPaint();
+		for (int angleStep = 0; angleStep < 128; ++angleStep) {
+			for (int lengthStep = 0; lengthStep < 16; ++lengthStep) {
+				QRgb col = pictureDataAVR[angleStep][lengthStep];
+				if (col != qRgb(0, 0, 0)) {
+					//if we would use a round cap instead of a flat cap, it would add half a sphere to the ends of the segments and color neighboring segments
+					drawSegment(angleStep, lengthStep, imageLEDLightDiameter, col, Qt::FlatCap);
+				}
+			}
+		}
+		image->save(filename, (const char*)nullptr, 100);
+		*image = image2;
+	}
 }
 void WCwindow::handleButtonLoad() {
-	textOut(QString("Load\n"));
+	//load an image from a file
+	QString filename = QFileDialog::getOpenFileName(this, "Open the image", "", "Images (*.png *.jpg *.jpeg *.bmp)");
+	
+	if (!filename.isEmpty()) {
+		clearImageNoPaint();
+		clearAVRData();
+		QImage image2;
+		image2.load(filename);
+		int size = image2.height() < image2.width() ? image2.height() : image2.width(); //choose the smaller side and make a square with it
+		for (int angleStep = 0; angleStep < 128; ++angleStep) {
+			for (int lengthStep = 0; lengthStep < 16; ++lengthStep) {
+				//for each angle and led take the corresponding pixel from the image and set the colors based on whether the pixel colors are over 255/2
+				int x = size / 2 + lengthStep * cosf(angleStep * 2 * M_PI / 128) * size / 2 / 16;
+				int y = size / 2 + lengthStep * sinf(angleStep * 2 * M_PI / 128) * size / 2 / 16;
+				QColor color = QColor(image2.pixel(x, y));
+				color.setRed(color.red() > 255 / 2 ? 255 : 0);
+				color.setGreen(color.green() > 255 / 2 ? 255 : 0);
+				color.setBlue(color.blue() > 255 / 2 ? 255 : 0);
+				if (color.rgb() != qRgb(0, 0, 0)){
+					//if the color was black, don't draw, because you will cover neighboring non-black segments, because the drawSegment function draws a little bit over the segment
+					drawSegment(angleStep, lengthStep, imageLEDLightDiameter, color.rgb());
+				}
+			}
+		}
+		imageHolder->setPixmap(QPixmap::fromImage(*image));
+	}
 }
 void WCwindow::handleCheckBoxes(int state) {
-	//change the color according to the checkboxes
+	//change the current drawing color according to the checkboxes
 	QColor colorNew(0,0,0);
 	if (checkBoxRed->isChecked()) {
 		colorNew.setRed(255);
@@ -156,27 +276,28 @@ void WCwindow::handleCheckBoxes(int state) {
 }
 
 bool WCwindow::eventFilter(QObject *obj, QEvent *event) {
+	//this function draws segments on the screen in response to mouse dragging and clicking
 	if (qobject_cast<QLabel*>(obj) == imageHolder) {
 		if (event->type() == QEvent::MouseButtonPress || event->type() == QEvent::MouseButtonDblClick || event->type() == QEvent::MouseMove) {
 			QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
 			if (mouseEvent->buttons() & Qt::LeftButton) {
 				if (event->type() == QEvent::MouseButtonPress || event->type() == QEvent::MouseButtonDblClick) {
+					//the mouse was clicked, so remember the last place where we clicked in the mouseMovementLine line
+					//this is needed because if we drag very fast, some points might get missed, so we keep in mind the last point and go through all the points in between by ourselves
 					mouseMovementLine->setP2(mouseEvent->pos());
-					drawSegment2(mouseMovementLine->x2(), mouseMovementLine->y2(), 7, activeColor);
+					drawSegmentXY(mouseMovementLine->x2(), mouseMovementLine->y2(), imageLEDLightDiameter, activeColor);
 					imageHolder->setPixmap(QPixmap::fromImage(*image));
 				}
 				else {
+					//the mouse was dragged while the button was down, make a line connecting the last and the current point and go through all the points inbetween and color the corresponding segments
 					mouseMovementLine->setP1(mouseMovementLine->p2());
 					mouseMovementLine->setP2(mouseEvent->pos());
 					for (float t = 0; t <= 1; t += 1 / mouseMovementLine->length()) {
 						QPointF point = mouseMovementLine->pointAt(t);
-						drawSegment2(point.x(), point.y(), 7, activeColor);
+						drawSegmentXY(point.x(), point.y(), imageLEDLightDiameter, activeColor);
 					}
 					imageHolder->setPixmap(QPixmap::fromImage(*image));
 				}
-
-				//textOut(QString("Coord:%1, %2\n").arg(QString::number(mouseEvent->pos().x()), QString::number(mouseEvent->pos().y())));
-				//drawSegment2(x, y, 7, qRgb(100, 100, 100));
 			}
 		}
 	}
@@ -184,13 +305,45 @@ bool WCwindow::eventFilter(QObject *obj, QEvent *event) {
 }
 
 void WCwindow::textOut(QString text1) {
+	//simply puts text to the big textedit
 	text->moveCursor(QTextCursor::End);
-	text->insertPlainText(text1); //, this->objectName, obj->objectName)
+	text->insertPlainText(text1);
 	text->ensureCursorVisible();
 }
 
-void WCwindow::drawSegment2(int x, int y, int diameter, QRgb color) {
-	//paints a segment corresponding to the point x,y, only changes the image file, doesn't display it
+void WCwindow::drawSegment(int angleStep, int lengthStep, int diameter, QRgb color, Qt::PenCapStyle penCapStyle) {
+	//paints the given segment the given color
+	QRectF rect(imageSize / 2 - lengthStep*ledPixelCount, imageSize / 2 - lengthStep*ledPixelCount,
+		2 * lengthStep*ledPixelCount, 2 * lengthStep*ledPixelCount);
+	QPainter painter(image);
+	painter.setPen(QPen(QColor(color), diameter, Qt::SolidLine, penCapStyle, Qt::RoundJoin));
+	pictureDataAVR[angleStep][lengthStep] = color;
+	if (lengthStep == 0) {
+		//if it is the first led in the middle, just draw a point
+		painter.drawPoint(imageSize / 2, imageSize / 2);
+	}
+	else {
+		//draw an arc representing the segment, rotate the segment by half a step, so that the midpoint is given by the angle in polar coordinates
+		painter.drawArc(rect, -16 * (angleStep + 0.5) * 360 / 128, 16 * 360 / 128);
+	}
+	if (color == qRgb(0, 0, 0)) {
+		//if we draw a black segment, redraw its neighboring segments so that we wont have a black segment covering colored segments on the sides, but the other way around
+		painter.end(); //needed so that we can draw again the old colors
+		int angleStep2 = (angleStep + 1) % 128;
+		QRgb col = pictureDataAVR[angleStep2][lengthStep];
+		if (col != qRgb(0, 0, 0)) {
+			drawSegment(angleStep2, lengthStep, diameter, col, penCapStyle);
+		}
+		angleStep2 = (angleStep + 128 - 1) % 128;
+		col = pictureDataAVR[angleStep2][lengthStep];
+		if (col != qRgb(0, 0, 0)) {
+			drawSegment(angleStep2, lengthStep, diameter, col, penCapStyle);
+		}
+	}
+}
+
+void WCwindow::drawSegmentXY(int x, int y, int diameter, QRgb color, Qt::PenCapStyle penCapStyle) {
+	//calculates the sgement where the point belongs to and paints it
 	int x1 = x - imageSize / 2;
 	int y1 = y - imageSize / 2; //x1 and y1 begin at the center and grow left and up
 	float angle = atan2f(y1, x1); //0 deg is on the x axis and goes clockwise
@@ -198,21 +351,9 @@ void WCwindow::drawSegment2(int x, int y, int diameter, QRgb color) {
 	int ledNumberFromTheCenter = roundf(distance / ledPixelCount);
 	int segmentNumber = roundf(64 * angle / (M_PI));
 	segmentNumber = segmentNumber < 0 ? 128 + segmentNumber : segmentNumber;
-	//textOut(QString("LED2: %1, segment: %2\n").arg(QString::number(ledNumberFromTheCenter), QString::number(segmentNumber)));
 	
-	if (ledNumberFromTheCenter < 16 && color != pictureDataAVR[segmentNumber][ledNumberFromTheCenter]) {
-		//if the color is different and the point isn't out of the display, paint it
-		QRectF rect(imageSize / 2 - ledNumberFromTheCenter*ledPixelCount, imageSize / 2 - ledNumberFromTheCenter*ledPixelCount,
-			2 * ledNumberFromTheCenter*ledPixelCount, 2 * ledNumberFromTheCenter*ledPixelCount);
-		QPainter painter(image);
-		painter.setPen(QPen(QColor(color), diameter, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
-		pictureDataAVR[segmentNumber][ledNumberFromTheCenter] = color;
-		if (ledNumberFromTheCenter == 0) {
-			painter.drawPoint(imageSize / 2, imageSize / 2);
-		}
-		else {
-			painter.drawArc(rect, -16 * segmentNumber * 360 / 128, 16 * 360 / 128);
-		}
+	if (ledNumberFromTheCenter < 16) {
+		drawSegment(segmentNumber, ledNumberFromTheCenter, diameter, color, penCapStyle);
 	}
 }
 
